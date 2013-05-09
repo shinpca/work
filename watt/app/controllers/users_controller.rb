a926b96b37750397c2ca7698b62a2f3adf91a6ac
shin <shin@shin-VirtualBox.(none)>
2013-05-09 19:55:02 +0900

    graph create

---
+++ watt/app/controllers/users_controller.rb
@@ -0,0 +1,7 @@
+class UsersController < ApplicationController
+  require 'fastercsv'
+
+  def graph
+    require 'fastercsv'
+  end
+end
--- watt/config/routes.rb
+++ watt/config/routes.rb
@@ -1,4 +1,6 @@
 Watt::Application.routes.draw do
+  get "users/graph"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
