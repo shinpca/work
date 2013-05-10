95ea169e29c24398ebbe0b993b6afd0bce856270
shin <shin@shin-VirtualBox.(none)>
2013-05-10 19:59:01 +0900

    gon setting

--- watt/app/controllers/users_controller.rb
+++ watt/app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
+# -*- coding: utf-8 -*-
 class UsersController < ApplicationController
-  require 'fastercsv'
 
   def graph
-    require 'fastercsv'
-  end
+  gon.graph = [
+['年度', '売上', '費用'],
+['2004', 1000, 400],
+['2005', 1170, 460],
+['2006', 660, 1120],
+['2007', 1030, 2000]
+]
+    end
 end
