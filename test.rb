0492abfaf08f12b2091883a22d65420876d2a556
shin <shin@shin-VirtualBox.(none)>
2013-05-17 11:25:34 +0900

    test.rb plus

--- test.rb
+++ test.rb
@@ -3,11 +3,11 @@ require 'open-uri'
 require 'csv'
 
 def read(url)
+ new_line []
  CSV.new(open(url), :headers => :first_row).each do |line|
-   puts line
-   puts line[0]
-   puts line['FEB11']
+    new_line << line
  end
+
 end
 
 read("http://www.euribor-ebf.eu/assets/modules/rateisblue/processed_files/myav_EURIBOR_2011.csv")
